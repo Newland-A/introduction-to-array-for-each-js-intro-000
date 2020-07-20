12f9ae43f8b709853c8064bedbf48c800d7231fc
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-20 15:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function doToElementsInArray(){
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(){
+function changeCompletely(callback){
   Array.prototype.forEach()
 }
