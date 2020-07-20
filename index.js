770769cad88b9c2537d69caf0c4bc12257aec7f2
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-20 15:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(){
-  Array.prototype.forEach()
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
