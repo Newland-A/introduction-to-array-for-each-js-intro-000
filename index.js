580f3366fb9a5c35fdd3b2cd0d0c0c6f681b146e
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-20 15:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function doToElementsInArray(){
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(array, callback){
-  Array.forEach(callback);
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
